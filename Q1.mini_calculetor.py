9c4bc73d47a6a1806d888bfde620298281ebd30e
Vaibhav koli <vaibhav212159c@gmail.com>
2026-09-06 16:23:07 +0530

    edit Q1

--- Q1.mini_calculetor.py
+++ Q1.mini_calculetor.py
@@ -4,9 +4,11 @@ b = int(input("Enter second number: "))
 add = a + b
 sub = a - b
 mul = a * b
-div = a / b
 
 print("Addition =", add,
       "\nSubtraction =", sub,
-      "\nMultiplication =", mul,
-      "\nDivision =", div)
+      "\nMultiplication =", mul)
+if b!=0:
+    print("divition =",a/b)
+else :
+    print("can't divide by 0 ")
